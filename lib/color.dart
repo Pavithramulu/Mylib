98c97229f0a2abe994b5b99fcdb93750c8bd7d73
Pavithramulu <128126638+Pavithramulu@users.noreply.github.com>
2023-05-09 14:46:01 +0530

    Add files via upload

---
+++ lib/color.dart
@@ -0,0 +1,10 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Color headwhite= Color(0xf5171617);
+
+TextStyle textas = TextStyle(color: Colors.white,fontSize: 18, fontWeight: FontWeight.w500);
+
+TextStyle styleas = TextStyle(color: Colors.white,fontSize: 18);
